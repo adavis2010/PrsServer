7b87a665914e27fcd49b8725ad1df9f684df9203
max-student <max-student@maxtrain.com>
2021-03-08 11:06:53 -0500

    Add project files.

---
+++ PrsServer/Data/PrsServerDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PrsServer.Models;
+
+namespace PrsServer.Data
+{
+    public class PrsServerDbContext : DbContext
+    {
+        public PrsServerDbContext (DbContextOptions<PrsServerDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder) { // makes identifier unique
+            }
+
+        }
+    }
+
---
+++ PrsServer/Models/User.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrsServer.Models {
+    public class User {
+        //default constructor
+        public User() { }
+
+        [Required]
+        public int Id { get; set; } //primary key
+
+        [StringLength(30),Required] //attribute
+        public string Username { get; set; }
+
+        [StringLength(30), Required] // attribute
+        public string Password { get; set; }
+
+        [StringLength(30), Required]//attribute
+        public string Firstname { get; set; }
+        
+        [StringLength(30), Required]//attribute
+        public string Lastname { get; set; }
+
+        [StringLength(12)]//attribute
+        public string Phone { get; set; }
+
+        [StringLength(255)]//attribute
+        public string Email { get; set; }
+
+        public bool IsReviewer { get; set; }
+
+        public bool IsAdmin { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
