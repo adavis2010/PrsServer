626742c9678032318edc2a169c3af463bc49e615
max-student <max-student@maxtrain.com>
2021-03-08 20:22:22 -0500

    updated requestline

---
+++ PrsServer/Migrations/20210309011654_requestline.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PrsServer.Migrations
+{
+    public partial class requestline : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Vendors");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Phone",
+                table: "Vendors",
+                maxLength: 12,
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Requestline",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RequestId = table.Column<int>(nullable: false),
+                    ProductId = table.Column<int>(nullable: false),
+                    Quantity = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Requestline", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Requestline_Product_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Product",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Requestline_Request_RequestId",
+                        column: x => x.RequestId,
+                        principalTable: "Request",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Requestline_ProductId",
+                table: "Requestline",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Requestline_RequestId",
+                table: "Requestline",
+                column: "RequestId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Requestline");
+
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "Vendors");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Vendors",
+                type: "nvarchar(12)",
+                maxLength: 12,
+                nullable: true);
+        }
+    }
+}
